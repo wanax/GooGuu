535d07d7510e0270df8ff93131e1aaaed48ccad4
wanax <mxiaochi@gmail.com>
2013-07-22 14:18:03 +0800

    Initial Commit

---
+++ 估股/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  估股
+//
+//  Created by Xcode on 13-7-22.
+//  Copyright (c) 2013年 Xcode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 估股/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  估股
+//
+//  Created by Xcode on 13-7-22.
+//  Copyright (c) 2013年 Xcode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
